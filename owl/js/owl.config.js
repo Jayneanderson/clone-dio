9fb22fbdfff5c9533c8eb1e707247ebebf6bf818
Jayneanderson Santos <aprenderprogramacao001@gmail.com>
2021-08-04 22:20:37 -0300

    Atualização: aqui eu mudei a exibição do carrossel para ser exibido 3 cards quando a tela estiver abaixo de 1200px e 2 abaixo de 580px

--- owl/js/owl.config.js
+++ owl/js/owl.config.js
@@ -8,6 +8,12 @@ owl.owlCarousel({
     navText: ['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
     responsive:{
         0: {
+            items: 2,
+            nav: true,
+            loop: false
+        },
+
+        580: {
             items: 3,
             nav: true,
             loop: false
